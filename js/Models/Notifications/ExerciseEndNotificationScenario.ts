ce39a43295240dc4c2a372438fabc13f90459bc8
Konstantin Kretov <exploit32@mail.ru>
2016-02-02 23:28:18 +0300

    #4 Added notification scenarios, NotificationBuilder and tests

---
+++ js/Models/Notifications/ExerciseEndNotificationScenario.ts
@@ -0,0 +1,27 @@
+/// <reference path="./INotificationScenario.d.ts" />
+/// <reference path="./INotification.d.ts" />
+/// <reference path="../IWorkoutPlanAction.d.ts" />
+/// <reference path="../IExcercisePlanAction.d.ts"/>
+
+import Workout from "../../Models/Workout";
+import SoundNotification from "./SoundNotification";
+import audioPlayer from "../../AudioPlayer/AudioPlayer";
+
+export default class ExerciseEndNotificationScenario implements INotificationScenario {
+
+    public createNotifications(workout: Workout): INotification[] {
+        let notifications: INotification[] = [];
+
+        for(var i in workout.actions) {
+            let action: IWorkoutPlanAction = workout.actions[i];
+
+            if(!workout.isActionRest(action)) {
+                notifications.push(new SoundNotification(
+                    workout.getRelativeActionStartTime(action)+action.duration,
+                    audioPlayer.getFiles().end))
+            }
+        }
+
+        return notifications;
+    }
+}
---
+++ js/Models/Notifications/PrepareToExerciseNotificationScenario.ts
@@ -0,0 +1,31 @@
+/// <reference path="./INotificationScenario.d.ts" />
+/// <reference path="./INotification.d.ts" />
+/// <reference path="../IWorkoutPlanAction.d.ts" />
+/// <reference path="../IExcercisePlanAction.d.ts"/>
+
+import Workout from "../../Models/Workout";
+import SoundNotification from "./SoundNotification";
+import audioPlayer from "../../AudioPlayer/AudioPlayer";
+
+export default class PrepareToExerciseNotificationScenario implements INotificationScenario {
+
+    private timeToPrepare: number = 5000;
+
+    public createNotifications(workout: Workout): INotification[] {
+        let notifications: INotification[] = [];
+
+        for(var i in workout.actions) {
+            let action: IWorkoutPlanAction = workout.actions[i];
+
+            if(workout.isActionRest(action)) {
+                let minTime: number = Math.min(this.timeToPrepare, action.duration);
+
+                notifications.push(new SoundNotification(
+                    workout.getRelativeActionStartTime(action) + action.duration - minTime,
+                    audioPlayer.getFiles().prepare))
+            }
+        }
+
+        return notifications;
+    }
+}
