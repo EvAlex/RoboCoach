e347762a5de4199b572ea9d62f6fa8e3e6e6fcc8
EvAlex <evalexhimself@gmail.com>
2016-02-16 01:10:05 +0300

    fix build

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -18,4 +18,3 @@
 /// <reference path="node-uuid/node-uuid-base.d.ts" />
 /// <reference path="node-uuid/node-uuid-cjs.d.ts" />
 /// <reference path="node-uuid/node-uuid.d.ts" />
-/// <reference path="uuid/UUID.d.ts" />
